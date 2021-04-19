193e142b36d4b22c613b10ea162602a68e504c6a
Martin Matych <martinmatych@gmail.com>
2021-04-19 11:59:10 +0200

    Add dna search improved

--- searching.py
+++ searching.py
@@ -41,8 +41,13 @@ def pattern_search(sequence, pattern):
     pos = set()
     idx = 0
     while idx < len(sequence) - len(pattern):
-        substring = sequence[idx:idx + len(pattern)]
-        if substring == pattern:
+        index = 0
+        while index < len(pattern):
+            if sequence[idx + index] == pattern[index]:
+                index = index +1
+            else:
+                break
+        else:
             pos.add(idx)
         idx = idx + 1
     return pos
